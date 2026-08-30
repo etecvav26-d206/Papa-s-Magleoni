21bd9ac464a23978e082645e3727161943c81dd1
Otávio Biazzi <223688772+OtavioBiazzi@users.noreply.github.com>
2026-08-30 18:55:23 -0300

    remove páginas sem uso

--- cadastro.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/auth.php';
-require_admin();
-header('Location: crud.php?entity=pizzas&action=create');
-exit;
--- editar.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/auth.php';
-require_admin();
-header('Location: crud.php?entity=pizzas&action=edit&id='.(int)($_GET['id']??0));
-exit;
